f08e8ee0e16a166513e4710bdfa1e5051a5ca423
ryangachahic <ryangachahi230@gmail.com>
2025-08-07 09:51:50 +0300

    intial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
-
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return []
+    
+    fib_sequence = [0, 1]
+    
+    if n == 1:
+        print([0])
+        return [0]
+    
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    
+    print(fib_sequence[:n])
+    return fib_sequence[:n]
